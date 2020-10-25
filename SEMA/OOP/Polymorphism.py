4135567ce6c342ec028581ee46de80ab886dc8ae
SemaBulut <sema.bulut.tedu@gmail.com>
2020-10-25 15:48:44 +0300

    25.10.20 3

--- SEMA/OOP/Polymorphism.py
+++ SEMA/OOP/Polymorphism.py
@@ -2,38 +2,57 @@
 
 #fonksiyonlarda polymorphism
 
-
-
 #def add(x,y,z=0):
    # return x+y+z
 
 #print(add(2,3))
 #print(add(2,3,4))
 
-class Azerbaycan:
-    def baskent(self):
-        print("Bakü Azerbaycan Başkentidir.")
-    
-    def dil(self):
-        print("Türkçe")
-    
-    def type(self):
-        print("Demokrasi")
+#class method polymorphism
 
-class Turkiye:
-    def baskent(self):
-        print("Ankara Türkiye Başkentidir.")
+#class Azerbaycan:
+#    def baskent(self):
+#        print("Bakü Azerbaycan Başkentidir.")
     
-    def dil(self):
-        print("Türkçe")
+#    def dil(self):
+#        print("Türkçe")
     
-    def type(self):
-        print("Demokrasi-Cumhuriyet")
+#    def type(self):
+#        print("Demokrasi")
 
-obj1 = Azerbaycan()
-obj2 = Turkiye()
+#class Turkiye:
+#    def baskent(self):
+#        print("Ankara Türkiye Başkentidir.")
+    
+#    def dil(self):
+#        print("Türkçe")
+    
+#    def type(self):
+#        print("Demokrasi-Cumhuriyet")
 
-for ulke in (obj1,obj2):
-    ulke.baskent()
-    ulke.dil()
-    ulke.type()
+#obj1 = Azerbaycan()
+#obj2 = Turkiye()
+
+#for ulke in (obj1,obj2):
+#    ulke.baskent()
+#    ulke.dil()
+#    ulke.type()
+
+class A():
+    def __init__(self):
+        self.a = "A"
+    def imet(self):
+        return self.a
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        self.b = "B"
+    def imet(self):
+        print(super().imet())
+        return self.b
+
+obj1 = B()
+print(obj1.imet())
+obj1 = A()
+print(obj1.imet())
