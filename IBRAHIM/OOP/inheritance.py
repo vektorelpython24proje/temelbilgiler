26bcf391ae189c0ee1d563771395ab77d75b378a
devEchore <73025471+devEchore@users.noreply.github.com>
2020-10-25 14:47:02 +0300

    ibrahim

--- IBRAHIM/OOP/inheritance.py
+++ IBRAHIM/OOP/inheritance.py
@@ -13,5 +13,10 @@ class B(A):
         super().__init__()
         self.b="B"
 
-obj1=B()
-obj1.imet()
+class C(B):
+    def __init__(self):
+        super().__init__()
+        self.c="C"
+
+obj1=C()
+obj1.imet2()
