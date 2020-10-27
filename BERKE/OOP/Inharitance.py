00fd9316dfce51a8ce107d43913f2cc2a34d3c9c
CuriousEngineerr <berke.kapukayapersonalComputer@hotmail.com>
2020-10-27 15:54:06 +0300

    Berke

--- BERKE/OOP/Inharitance.py
+++ BERKE/OOP/Inharitance.py
@@ -1,19 +1,28 @@
+# inharitance
+
 class A:
     def __init__(self):
         self.a = "A"
-
+    
     def imet(self):
         print("Merhaba")
-
+    
     def imet2(self):
         print(self.a)
+
 
 class B(A):
     def __init__(self):
         super().__init__()
         self.b = "B"
 
-obj1 = B()
+class C(B):
+    def __init__(self):
+        super().__init__()
+        self.c = "C"
 
-obj1.imet()
+
+        
+
+obj1 = A()
 obj1.imet2()
