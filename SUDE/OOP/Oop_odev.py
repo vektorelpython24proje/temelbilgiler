8dfbbe202874fa811bcdae4f43be5d71ff91fd6a
sudeyalcinkaya <sudeyalcinkaya98@hotmail.com>
2020-10-29 21:00:08 +0300

    bitmedihala

--- SUDE/OOP/Oop_odev.py
+++ SUDE/OOP/Oop_odev.py
@@ -1,44 +1,73 @@
 ### DAHA BİTMEDİ
+from abc import ABC, abstractmethod
 
-class ARAC():
-  "Ulaşım veya taşıma amaçlı üretilen araçlar sınıfı"
-    def __init__(self,tur,yakit,kapasite,hız):
+class ARAC(ABC):
+    "Ulaşım veya taşıma amaçlı üretilen araçlar sınıfı"
+    
+    def __init__(self,tur,yakit,kapasite,hiz):
         self.tur = tur
         self.yakit = yakit
         self.kapasite = kapasite
-        self.hız = hız
+        self.hiz = hiz
 
+    @abstractmethod
     def neTur(self):
         return "{} tipi yakıt kullanan {}'nda seyredebilen araçtır".format(self.yakit,self.tur)
-
+    @abstractmethod
     def neYapar(self):
-        return "{} yolcuyu {} hız ile taşır".format(self.kapasite,self.hız)
+        return "{} yolcuyu {} hız ile taşır".format(self.kapasite,self.hiz)   
+        
 
 
 class Karayolu(ARAC):
     "Karayolunda gidebilen araçlar sınıfı"
-    def __init__(self,tur,yakit,kapasite,hız,marka,renk,cins):
-        super().__init__(tur,yakıt,kapasite,hız)
+    def __init__(self,tur,yakit,kapasite,hiz,marka,renk,cins):
+        super().__init__(tur,yakit,kapasite,hiz)
         self.marka=marka
         self.renk=renk
         self.cins=cins
+    
+    def neTur(self):
+        return "{} tipi yakıt kullanan {}'nda seyredebilen araçtır".format(self.yakit,self.tur)
+    
+    def neYapar(self):
+        return "{} yolcuyu {} hız ile taşır".format(self.kapasite,self.hiz)
+
+
 class Denizyolu(ARAC):
     "Denizyolunda gidebilen araçlar sınıfı"
-    def __init__(self,tur,yakit,kapasite,hız,kaptan,renk,cins):
-        super().__init__(tur,yakıt,kapasite,hız)
+    def __init__(self,tur,yakit,kapasite,hiz,kaptan,renk,cins):
+        super().__init__(tur,yakit,kapasite,hiz)
         self.kaptan=kaptan
         self.renk=renk
         self.cins=cins
+    
+    def neTur(self):
+        return "{} tipi yakıt kullanan {}'nda seyredebilen araçtır".format(self.yakit,self.tur)
+    
+    def neYapar(self):
+        return "{} yolcuyu {} hız ile taşır".format(self.kapasite,self.hiz)
+
+
 class Havayolu(ARAC):
     "Havayolunda gidebilen araçlar sınıfı"
-    def __init__(self,tur,yakit,kapasite,hız,pilot,renk,cins):
-        super().__init__(tur,yakıt,kapasite,hız)
+    def __init__(self,tur,yakit,kapasite,hiz,pilot,renk,cins):
+        super().__init__(tur,yakit,kapasite,hiz)
         self.pilot=pilot
         self.renk=renk
         self.cins=cins
+    
+    def neTur(self):
+        return "{} tipi yakıt kullanan {}'nda seyredebilen araçtır".format(self.yakit,self.tur)
+    
+    def neYapar(self):
+        return "{} yolcuyu {} hız ile taşır".format(self.kapasite,self.hiz)
 
-otomobil1= ARAC("karayolu","benzin",5,"250km/h")
-uçak1=ARAC("havayolu","mazot",150,"450km/h")
-yat1=ARAC("denizyolu","blendoil",5000,"19knot")^
+
+def genelBilgi(arac):
+    arac.neTur()
+    arac.neYapar()
+
+otomobil = Karayolu()
 
 ### DAHA BİTMEDİ 
