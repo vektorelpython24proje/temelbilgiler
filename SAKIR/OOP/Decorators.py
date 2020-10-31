fb561c7fdffc9b94b01631c466677aeee683d3ec
vektorel <vektorel@LAB11-3>
2020-10-31 16:49:10 +0200

    SAKIR 31102020 son

--- SAKIR/OOP/Decorators.py
+++ SAKIR/OOP/Decorators.py
@@ -9,6 +9,7 @@ class A:
 obj1 = A()
 print(obj1.pi())
 
+
 import time
 
 def hesapZaman(fonk):
@@ -26,11 +27,11 @@ def birlestirCon(*args):
     for item in args:
         sonuc += " " + item
     print(sonuc)
-
 @hesapZaman
 def birlestirJoin(*args):
     time.sleep(1)
     return " ".join(args)
+
 
 birlestirCon("UWUWEWEWE", "ONYETENWEWE","UGWEUHEM","OSAS")
 birlestirJoin("UWUWEWEWE", "ONYETENWEWE","UGWEUHEM","OSAS")
