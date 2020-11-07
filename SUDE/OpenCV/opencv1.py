c7803a95ef165cbd44e90b01a0507e8ba990c3f7
sudeyalcinkaya <sudeyalcinkaya98@hotmail.com>
2020-11-07 14:02:05 +0300

    yallah

--- SUDE/OpenCV/opencv1.py
+++ SUDE/OpenCV/opencv1.py
@@ -1,5 +1,6 @@
 import cv2
-img=cv2.imread("EDIZ\OPENCV\resim.png")
-cv2.imshow("ilresim",img)
+img = cv2.imread(r"EDIZ\OPENCV\resim.png")
+print(img[100:150,100:150])
+cv2.imshow("ilkresim",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
