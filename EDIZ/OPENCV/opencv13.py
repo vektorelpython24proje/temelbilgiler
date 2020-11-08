a033b0eb6c91468ea15640cea5d14e7545058e26
ibrahimediz <vektorelpython24@gmail.com>
2020-11-08 16:08:12 +0200

    08112020

--- EDIZ/OPENCV/opencv13.py
+++ EDIZ/OPENCV/opencv13.py
@@ -11,6 +11,12 @@ while True:
     yuzler = yuz_cas.detectMultiScale(gri,1.3,5)
     for x,y,w,h in yuzler:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+        roi = frame[y:y+h,x:x+w]
+        roigri = gri[y:y+h,x:x+w]
+        gozler = eye_cas.detectMultiScale(roigri)
+        for ex,ey,ew,eh in gozler:
+            cv2.rectangle(roi,(ex,ey),(ex+ew,ey+eh),(255,0,0),1)
+
 
     cv2.imshow("resim",frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
