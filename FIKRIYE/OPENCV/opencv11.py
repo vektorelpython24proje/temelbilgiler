29b5e4a0d92729900ca5b8a94fe89120f3e981df
33-Fikriye <73080436+33-Fikriye@users.noreply.github.com>
2020-11-09 22:18:52 +0200

    commit

---
+++ FIKRIYE/OPENCV/opencv11.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+img = cv2.imread(r"EDIZ\OPENCV\basin.jpg")
+gri = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+temp1 = cv2.imread(r"EDIZ\OPENCV\temp1.jpg",0)
+temp2 = cv2.imread(r"EDIZ\OPENCV\temp2.jpg",0)
+w,h = temp2.shape[::-1]
+res = cv2.matchTemplate(gri,temp2,cv2.TM_CCOEFF_NORMED)
+thresh = 0.5
+loc = np.where(res>=thresh)
+# print(zip(*loc[::-1]))
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img,pt,(pt[0]+w,pt[1]+h),(0,255,255),2)
+
+
+
+cv2.imshow("ilkresim",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
