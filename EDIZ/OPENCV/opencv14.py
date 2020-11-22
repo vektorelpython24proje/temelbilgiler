8a29a7936d47f769851ec1a5130d0d25512134b6
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-11-22 22:06:44 +0300

    22112020

--- EDIZ/OPENCV/opencv14.py
+++ EDIZ/OPENCV/opencv14.py
@@ -10,8 +10,6 @@ def transparentOverlay(src, overlay, pos=(0, 0), scale=1):
     h, w, _ = overlay.shape  # Size of foreground
     rows, cols, _ = src.shape  # Size of background Image
     y, x = pos[0], pos[1]  # Position of foreground/overlay image
- 
-    # loop over all pixels and apply the blending equation
     for i in range(h):
         for j in range(w):
             if x + i >= rows or y + j >= cols:
@@ -25,17 +23,11 @@ while True:
     if take:
         ret,frame = cap.read()
         gri = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        
-        # yuzler = yuz_cas.detectMultiScale(gri,1.3,5)
-
-
-
-
         cv2.imshow("resim",frame)
     else:
         cv2.imshow("resim",res)
     if cv2.waitKey(2) & 0xFF == ord("t"):
-        yuzler = yuz_cas.detectMultiScale(gri,1.3,5)
+        yuzler = yuz_cas.detectMultiScale(gri,1.1,5)
         for x,y,w,h in yuzler:
             if h>0 and w>0:
                 gozluk_min = int(y + 1.5*h/5)
@@ -49,7 +41,7 @@ while True:
         res = frame.copy()
         from pygame import mixer  # Load the popular external library
         mixer.init()
-        mixer.music.load(r'EDIZ\OPENCV\thuglife.mp3')
+        mixer.music.load(r'EDIZ\OPENCV\zil.mp3')
         mixer.music.play()
     if cv2.waitKey(2) & 0xFF == ord("s"):
         take = True
@@ -58,7 +50,7 @@ while True:
         except:
             pass        
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    if cv2.waitKey(4) & 0xFF == ord("q"):
         break
 
 cap.release()
